7531335836da63c670b21fbe9ac13cf65b0a2e02
Evgeniy_Utkin <eugene.utkin@gmail.com>
2020-08-14 19:28:17 +0500

    Detailed View Added

--- app.rb
+++ app.rb
@@ -47,5 +47,9 @@ end
 
 get '/details/:post_id' do
   post_id = params[:post_id]
-  erb "Displaying information for post with id #{post_id}"
+
+  results = @db.execute 'SELECT * FROM Posts WHERE id = ?', [post_id]
+  @row = results[0]
+
+  erb :details
 end
