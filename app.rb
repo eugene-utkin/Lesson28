12d476df8df1222b6d4c90b48a37376d0299c471
Evgeniy_Utkin <eugene.utkin@gmail.com>
2020-08-14 17:11:56 +0500

    Database Init Added

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3' 
+
+def init_db
+  @db = SQLite3::Database.new 'blogposts.db'
+  @db.results_as_hash = true
+end
+
+before do
+
+end
 
 get '/' do
   erb "Hello!"  
@@ -11,6 +21,7 @@ get '/new' do
 end
 
 post '/new' do
+  @db.execute
   content = params[:content]
   erb "You typed #{content}"
 end
